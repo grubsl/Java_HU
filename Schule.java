7d3e88ed9524be3da418712f0c673754b2479522
Alex <alexanderwippel000@gmail.com>
2018-12-10 16:17:00 +0100

    Update Schule.java

--- Schule.java
+++ Schule.java
@@ -6,6 +6,39 @@ public class Schule
 		String name="";
 		long int schulkennzahl=0;
 		String schultyp="";
-
+		
+		public:
+			
+		void addPersonal(NichtLehrpersonal personal)
+		{
+			
+		}
+		
+		void addAbteilung(String name, String kuerzel)
+		{
+			
+		}
+		
+		void setDirektor(Lehrer lehrer)
+		{
+			
+		}
+		
+		void getSchulkennzahl(long int zahl)
+		{
+			this.zahl=schulkennzahl;
+		}
+		
+		void getName(String nameh)
+		{
+			this.nameh=name;
+		}
+		
+		void getSchultyp(String schultyph)
+		{
+			
+		}
+		
+		void getPersonal()
 	}
 }

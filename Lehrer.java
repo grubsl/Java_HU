a607ba1cb53f1ce97b8041c45ac5f444046ccd43
AlexanderWippel <45587576+AlexanderWippel@users.noreply.github.com>
2019-01-04 19:35:06 +0100

    Lehrerkonstrucktor erweitert

--- Lehrer.java
+++ Lehrer.java
@@ -18,12 +18,14 @@ public class Lehrer extends Mitarbeiter
 	private List<Fach> faecher = new ArrayList<Fach>();
 	
 	public Lehrer(){}
-	public Lehrer(Long svnr, String vorname, String nachname, Date geburtsdatum, String email, String kuerzel,Abteilung Abteilung)
+	public Lehrer(Long svnr, String vorname, String nachname, Date geburtsdatum, String email, String kuerzel,Abteilung Abteilung,Klasse klassenvorstand1,Klasse klassenvorstand2)
 	{
 		super(svnr, vorname, nachname, geburtsdatum, email);
 		Aufgabe = false;
 		this.kuerzel = kuerzel;
 		this.Abteilung = Abteilung;
+		vorstand[0]=klassenvorstand1;
+		vorstand[1]=klassenvorstand2;
 	}
 
 	public String getKuerzel()
